c4e96a1726858e29d70f78312dc292c0e2e5301b
rvwdude <rvwdude@gmail.com>
2019-05-06 09:48:42 +0300

    fs-slider

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,14 @@
 import '../styles/index.scss';
+import '../../node_modules/tiny-slider/src/tiny-slider.scss';
 
 
+import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
+
+var slider = tns({
+  container: '.fs-slider',
+  "items": 1,
+  "speed": 400,
+  controls: true,
+  "controlsContainer": ".fs-slider__controls",
+  nav: false
+});
