00088e2733fadfe03612e1a05243ba0c4f6007fe
rvwdude <rvwdude@gmail.com>
2019-05-06 10:58:23 +0300

    slider-a

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,26 @@
-import '../styles/index.scss';
-import '../../node_modules/tiny-slider/src/tiny-slider.scss';
-
+import "../styles/index.scss";
+import "../../node_modules/tiny-slider/src/tiny-slider.scss";
 
 import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
 
 var slider = tns({
-  container: '.fs-slider',
-  "items": 1,
-  "speed": 400,
+  container: ".slider-a",
+  items: 4,
+  speed: 400,
+  gutter: 20,
   controls: true,
-  "controlsContainer": ".fs-slider__controls",
-  nav: false
+  controlsContainer: ".slider-a__controls",
+  nav: false,
+
+  "responsive": {
+    "1200": {
+      "items": 4
+    },
+    "960": {
+      "items": 2
+    },
+    "0": {
+      "items": 1
+    }
+  }
 });
